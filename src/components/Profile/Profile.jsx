721cdf7172abab50d74b06b91a8439004b6c30e6
mepinit <evgenmep@gmail.com>
2024-02-11 23:17:14 +0200

    1 task ++

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,46 +1,32 @@
-import PropTypes from 'prop-types';
-import css from 'components/Profile/profile.module.css';
+import css from "./Profile.module.css";
 
-export const Profile = ({ username, tag, location, avatar, stats }) => {
+export const Profile = ({ image, name, tag, location, stats }) => {
   return (
-    <div className={css.profile}>
-      <div className={css.description}>
+    <div className={}>
+      <div className={}>
         <img
-          src={avatar}
-          alt={username}
-          className={css.avatar}
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
         />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag}>{'@' + tag}</p>
-        <p className={css.location}>{location}</p>
+        <p className={}>Petra Marica</p>
+        <p className={}>@pmarica</p>
+        <p className={}>Salvador, Brasil</p>
       </div>
 
-      <ul className={css.stats}>
-        <li className={css.item}>
-          <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
+      <ul className={}>
+        <li className={}>
+          <span className={}>Followers</span>
+          <span className={}>1000</span>
         </li>
-        <li className={css.item}>
-          <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
+        <li className={}>
+          <span className={}>Views</span>
+          <span className={}>2000</span>
         </li>
-        <li className={css.item}>
-          <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
+        <li className={}>
+          <span className={}>Likes</span>
+          <span className={}>3000</span>
         </li>
       </ul>
     </div>
-  )
-}
-
-Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
-  }).isRequired,
-}
+  );
+};
